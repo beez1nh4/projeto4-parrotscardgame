af0b8f034f277821f51b39bd210c8fc7133b5648
Melissa <melissahentona@usp.br>
2022-08-20 10:58:21 -0300

    layout pronto + perguntaCarta + distribuiCarta + aleatoriedade

---
+++ script.js
@@ -0,0 +1,46 @@
+let numero;
+let divCartas;
+let cartas =[];
+let desenhos = ['bobrosparrot.gif','explodyparrot.gif','fiestaparrot.gif', 'metalparrot.gif','revertirparrot.gif','triplets.parrot.gif','unicornparrot.gif']
+
+//pergunta quantas cartas quer jogar
+function quantasCartas(){
+    numero = 1;   
+    while (numero%2 !== 0 || numero < 3 || numero > 14){4
+        numero = prompt("Com quantas cartas você quer jogar? (escolha um número par de 4 a 14)");
+    }
+}
+quantasCartas();
+
+//distribui as cartas de acordo com numero
+function distribuiCartas(){
+    divCartas = document.querySelector('.cartas');
+    for (let i = 0; i < numero;i++){
+        let cartinha = divCartas.children[i];
+        cartinha.classList.remove('escondida');
+    }
+}
+distribuiCartas();
+
+//cria uma lista de cartas com os desenhos
+function criaLista(){
+    for (let j = 0; j < (numero/2);j++){
+        cartas.push(desenhos[j]);
+        cartas.push(desenhos[j]);
+    }
+}
+criaLista();
+
+//embaralha a lista (função dada)
+cartas.sort(comparador);
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+//mostrar o desenho quando clicar na carta
+function virarCarta(cartaClicada){
+    let contador = 0;
+    while (cartaClicada != divCartas.children[contador]){
+        contador ++;
+    }
+}
